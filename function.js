51e99db6f81717b2c8619417379e76f75a007f8f
taaro <demal2525@gmail.com>
2025-06-30 13:12:49 +0600

    function in multiple code/program

--- function.js
+++ function.js
@@ -1,5 +1,6 @@
 
 function reversedString(str){
+    //return str.split("").reverse().join("");
     result ='';
     for (let i=str.length-1; i>=0; i--){
         result += str[i];
@@ -7,3 +8,52 @@ function reversedString(str){
     return result;
 }
 console.log(reversedString("hello"))
+
+
+// //"Hello", "l", "0" -> "he00o"
+function replace(str, charToReplace, charToReplacement){
+    let result ="";
+    for (let i=0; i<str.length; i++){
+        if (str[i] === charToReplace){
+            result += charToReplacement ;
+        }
+        else{
+            result += str[i];
+        }
+    }
+    return result;
+}
+console.log(replace("hello","l","p"))
+
+// //find out the given string is even or odd
+
+function strLengthEven(str){
+    let length ='';
+    for(let i= 0; i<str.length; i++){
+        length = str.length[i];
+        if(str.length %2 === 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    return length;
+}
+console.log(strLengthEven("hello"))
+console.log(strLengthEven("even"))
+
+//find out whether given array have rabbit or not?
+
+function findRabiit(arr){
+    //return arr.toUppercase().indexOf("rabbit");
+    for (let i = 0; i < arr.length; i++) {
+        // Convert to lowercase for case-insensitive search
+        if (arr[i].toLowerCase().includes("rabbit")) {
+            return i; // return the index where 'rabbit' is found
+        }
+    }
+    return -1; // 
+}
+console.log(findRabiit(["rabbit","dog","cat"]));
+console.log(findRabiit(["dog","cat","rabbit"]))
