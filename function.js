cb5eba406aa1398f40fe28f4b845ff5a5852bda8
taaro <demal2525@gmail.com>
2025-07-01 12:33:06 +0600

    add function to countPositiveSumNegatives

--- function.js
+++ function.js
@@ -60,3 +60,22 @@ function findRabiit(arr){
 }
 console.log(findRabiit(["dog","cat","tiger","elephat","Rabbit"]));
 console.log(findRabiit(["dog","cat","Rabbit"]));
+
+function countPositivesSumNegatives(input) {
+    
+    //return input.length;
+  let count = 0;
+  let sum = 0;
+  if(!input || input.length === 0) return [];
+  for(let i=0; i<input.length; i++){
+if(input[i] >0){
+    count += 1;
+}
+if(input[i]<0){
+    sum += input[i];
+}
+  }
+  return [count,sum];
+
+}
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
